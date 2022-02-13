759aa9244f58b39581567d8becf8839f89b8e4bb
Tulio Colmanetti <tulio.colmanetti@gmail.com>
2022-02-13 18:26:59 -0300

    Added validation Beans to class properties

--- src/main/java/br/com/letscode/rebelalliance/domain/Rebel.java
+++ src/main/java/br/com/letscode/rebelalliance/domain/Rebel.java
@@ -1,6 +1,9 @@
 package br.com.letscode.rebelalliance.domain;
 
 import br.com.letscode.rebelalliance.enums.Race;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -9,7 +12,16 @@ import lombok.Data;
 @AllArgsConstructor
 @Builder
 public class Rebel {
+    @NotNull
+    @NotEmpty
     private String Name;
+
+    @NotNull
+    @NotEmpty
+    @Min(value = 0)
     private int Age;
+
+    @NotNull
+    @NotEmpty
     private Race Race;
 }
