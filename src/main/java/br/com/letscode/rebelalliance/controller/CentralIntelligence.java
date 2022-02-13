be95faeb0269799a9c8da40d7900a013e56a8e8f
Tulio Colmanetti <tulio.colmanetti@gmail.com>
2022-02-13 12:41:44 -0300

    Changed list implementation to LinkedList and insert method used.

--- src/main/java/br/com/letscode/rebelalliance/controller/CentralIntelligence.java
+++ src/main/java/br/com/letscode/rebelalliance/controller/CentralIntelligence.java
@@ -7,23 +7,22 @@ import lombok.Getter;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.LinkedList;
 import java.util.Random;
 
 public class CentralIntelligence {
     @Getter
-    List<Rebel> rebels;
+    LinkedList<Rebel> rebels;
 
     public CentralIntelligence(){
-        this.rebels = new ArrayList<>();
+        this.rebels = new LinkedList<>();
     }
 
     public boolean requestToJoinAlliance(Rebel rebel) {
         int randNumber = new Random().nextInt(10);
 
         if(randNumber <= 5) {
-            this.rebels.add(rebel);
+            this.rebels.push(rebel);
             return true;
         }
         else
